4abecc014392c35bc7f5f5c4fb3356e6375b6eca
Kauan Mattos <159142117+kauanmattos@users.noreply.github.com>
2025-02-24 17:41:38 -0300

    Atualização das Notas

--- appNotas/src/app/home/home.page.ts
+++ appNotas/src/app/home/home.page.ts
@@ -8,6 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  notaUm = 0;
+  notaDois = 0;
+  notaTres = 0;
+  notaQuatro = 0;
+  resultado = '';
+
   constructor() {}
 
+   calcularMedia(){
+    const media = (this.notaUm + this.notaDois + this.notaTres + this.notaQuatro) / 4
+
+    if (media >= 6) {
+      this.resultado = 'Você Passou!';
+    } else if (media >= 3 && media <= 5.9) {
+      this.resultado = 'Ficou de Recuperação.';
+    } else {
+      this.resultado = 'Retido';
+    }
+  }
 }

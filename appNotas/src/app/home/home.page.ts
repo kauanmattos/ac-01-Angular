b8ee194a2798f06cd1742e20fe3c3b6d4c724bdc
Kauan Mattos <159142117+kauanmattos@users.noreply.github.com>
2025-02-25 09:16:18 -0300

    Update home.page.ts

--- appNotas/src/app/home/home.page.ts
+++ appNotas/src/app/home/home.page.ts
@@ -8,23 +8,76 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  // Convertor De Temperatura - EX 01
+  valorC = 0;
+  valorF = 0;
+  valorConvertidoPraC = 0;
+  valorConvertidoPraF = 0;
+
+  // Verificação de Triângulo - EX 02
+  lado1 = '';
+  lado2 = '';
+  lado3 = '';
+  res = ''
+
+  // Calculadora de Notas - EX 03
   notaUm = 0;
   notaDois = 0;
   notaTres = 0;
   notaQuatro = 0;
   resultado = '';
 
-  constructor() {}
+  constructor() { }
 
-   calcularMedia(){
-    const media = (this.notaUm + this.notaDois + this.notaTres + this.notaQuatro) / 4
+  
+  //EX 01
+  converterPraC() {
+    this.valorConvertidoPraC = (this.valorF - 32) * (5 / 9)
+  }
+  converterPraF() {
+    this.valorConvertidoPraF = (9 * this.valorC + 160) / 5;
+  }
 
-    if (media >= 6) {
-      this.resultado = 'Você Passou!';
-    } else if (media >= 3 && media <= 5.9) {
-      this.resultado = 'Ficou de Recuperação.';
-    } else {
-      this.resultado = 'Retido';
+  //EX 02
+
+  tipoTriangulo(){
+    if (this.lado1 + this.lado2 > this.lado3 &&
+      this.lado1 + this.lado3 > this.lado2 &&
+      this.lado2 + this.lado3 > this.lado1) {
+      if (this.lado1 === this.lado2 && this.lado2 === this.lado3) {
+        this.res = 'Triângulo Equilátero';
+      } else if (this.lado1 === this.lado2 || this.lado1 === this.lado3 || this.lado2 === this.lado3) {
+        this.res = 'Triângulo Isósceles';
+      } else {
+        this.res = 'Triângulo Escaleno';
+      }
     }
   }
+
+  verificarTriangulo(){
+    if (this.lado1 + this.lado2 > this.lado3 &&
+      this.lado1 + this.lado3 > this.lado2 &&
+      this.lado2 + this.lado3 > this.lado1) {
+      this.res = 'É um Triângulo.'
+  } else {
+    this.res = 'Não é um Triângulo.'
+  }
 }
+
+
+//EX 03
+
+calcularMedia() {
+  const media = (this.notaUm + this.notaDois + this.notaTres + this.notaQuatro) / 4
+
+  if (media >= 6) {
+    this.resultado = `Você Passou! sua nota foi: ${media}`;
+  } else if (media >= 3 && media <= 5.9) {
+    this.resultado =  `Vocêficou de recuperação sua nota foi: ${media}`;
+  } else {
+    this.resultado =  `Você ficou retido, sua nota foi: ${media}`;
+  }
+}
+}
+
+
